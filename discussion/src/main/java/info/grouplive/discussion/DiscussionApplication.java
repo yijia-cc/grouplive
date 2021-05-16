f30d1d6b118f6b3f106c37dc918d3ea912de2a00
yijiacc <76569613+yijia-cc@users.noreply.github.com>
2021-05-15 19:17:49 -0700

    Add test route for spring (#18)
    * Configure CD for Springboot project
    
    * Test
    
    * Test
    
    * Clean up
    
    * Clean up
    
    * Add default route & port
    
    * Update discussion/build.gradle

--- discussion/src/main/java/info/grouplive/discussion/DiscussionApplication.java
+++ discussion/src/main/java/info/grouplive/discussion/DiscussionApplication.java
@@ -2,7 +2,10 @@ package info.grouplive.discussion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DiscussionApplication {
 
@@ -10,4 +13,8 @@ public class DiscussionApplication {
 		SpringApplication.run(DiscussionApplication.class, args);
 	}
 
+	@GetMapping(value ="/hello")
+	public String hello() {
+		return "Hello World!";
+	}
 }

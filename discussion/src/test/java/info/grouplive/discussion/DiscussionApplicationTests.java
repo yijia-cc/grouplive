2ef13c950f8d4b652773c002ace26a2cffd4a3af
yijia-cc <bestmissjia@gmail.com>
2021-05-15 18:00:22 -0700

    Test

--- discussion/src/test/java/info/grouplive/discussion/DiscussionApplicationTests.java
+++ discussion/src/test/java/info/grouplive/discussion/DiscussionApplicationTests.java
@@ -2,12 +2,13 @@ package info.grouplive.discussion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api;
 
 @SpringBootTest
 class DiscussionApplicationTests {
 
 	@Test
 	void contextLoads() {
+	    fail("Build test fail!");
 	}
-
 }

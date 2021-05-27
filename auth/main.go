8d3151828d024c2e17f2f14c4f3d6690d97b0aac
qingqingkang <isabellakqq@gmail.com>
2021-05-26 23:21:53 -0700

    Add log

--- auth/main.go
+++ auth/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/yijia-cc/grouplive/auth/config"
 	"github.com/yijia-cc/grouplive/auth/controller"
 	"github.com/yijia-cc/grouplive/auth/model"
 	"log"
 	"net/http"
+	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -19,5 +21,12 @@ func main() {
 
 	router := controller.StartUp(cfg)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Fatal(http.ListenAndServe(":8080", router))
+	}()
+	fmt.Println("Service started at :8080")
+	wg.Wait()
 }

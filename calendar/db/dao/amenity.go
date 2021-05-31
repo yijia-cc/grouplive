1704b6b62fb64c4b388ea630d4e6d221eb35a711
yijia-cc <bestmissjia@gmail.com>
2021-05-31 00:42:32 -0700

    Implement DAOs

--- calendar/db/dao/amenity.go
+++ calendar/db/dao/amenity.go
@@ -17,7 +17,30 @@ type AmenitySQL struct {
 }
 
 func (a AmenitySQL) FindAmenityInfo(transaction tx.Transaction, amenityTypeId entity.ID) ([]entity.AmenityInfo, error) {
-	panic("implement me")
+	stmt, err := transaction.DBTransaction.Prepare(
+		`
+SELECT id, name, type_id 
+FROM amenity 
+WHERE type_id = "?";
+`)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := stmt.Query(amenityTypeId)
+	if err != nil {
+		return nil, err
+	}
+
+	amenityInfos := make([]entity.AmenityInfo, 0)
+	for rows.Next() {
+		amenityInfo := entity.AmenityInfo{}
+		err = rows.Scan(&amenityInfo.ID, &amenityInfo.Name, &amenityInfo.AmenityTypeID)
+		if err != nil {
+			continue
+		}
+		amenityInfos = append(amenityInfos, amenityInfo)
+	}
+	return amenityInfos, nil
 }
 
 func NewAmenitySQL(db *sql.DB) AmenitySQL {
--- calendar/db/dao/amenitytype.go
+++ calendar/db/dao/amenitytype.go
@@ -17,7 +17,25 @@ type AmenityTypeSQL struct {
 }
 
 func (a AmenityTypeSQL) GetAllAmenityTypes(transaction tx.Transaction) ([]entity.AmenityType, error) {
-	panic("implement me")
+	rows, err := transaction.DBTransaction.Query(`
+SELECT id, title, description, thumbnail_url 
+FROM amenity_type;
+`)
+	if err != nil {
+		return nil, err
+	}
+
+	amenityTypes := make([]entity.AmenityType, 0)
+
+	for rows.Next() {
+		amenityType := entity.AmenityType{}
+		err = rows.Scan(&amenityType.ID, &amenityType.Title, &amenityType.Description, &amenityType.ThumbnailURL)
+		if err != nil {
+			continue
+		}
+		amenityTypes = append(amenityTypes, amenityType)
+	}
+	return amenityTypes, nil
 }
 
 func NewAmenityTypeSQL(db *sql.DB) AmenityTypeSQL {

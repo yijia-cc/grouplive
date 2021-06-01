9e219073a8b4a94fdb34d8428905e316912f6dbc
Qingqing Kang <qingqingkang@Qingqings-MacBook-Pro.local>
2021-06-01 12:53:39 -0700

    Add userHandler and data model

--- dashboard/main.go
+++ dashboard/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
     "fmt"
+    _ "github.com/go-sql-driver/mysql"
+    "github.com/yijia-cc/grouplive/dashboard/config"
+    "github.com/yijia-cc/grouplive/dashboard/controller"
+    "github.com/yijia-cc/grouplive/dashboard/model"
     "log"
     "net/http"
+    "sync"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello Dashboard!")
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Server started at 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    userDBConfig, dashDBConfig := config.LoadEnv()
+    userDB, dashDB := model.DBConn(userDBConfig, dashDBConfig)
+    defer userDB.Close()
+    defer dashDB.Close()
+
+    router := controller.StartUp(dashDBConfig)
+
+    wg := sync.WaitGroup{}
+    wg.Add(1)
+    go func() {
+        defer wg.Done()
+        log.Fatal(http.ListenAndServe(":9090", router))
+    }()
+    fmt.Println("Dashboard Service started at :9090")
+    wg.Wait()
 }

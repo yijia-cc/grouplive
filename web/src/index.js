97ff7b62430ab7bad6623f98a32a38efbab70da7
Jenny Zhan <34534797+Zanzan666@users.noreply.github.com>
2021-05-31 18:45:14 -0700

    added ui (#65)

--- web/src/index.js
+++ web/src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 

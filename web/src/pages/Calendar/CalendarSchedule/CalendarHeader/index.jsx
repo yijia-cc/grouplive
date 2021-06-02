03dec3d09a39569c6e42e3d8934494309864f49f
wl328Weiminli <54866038+wl328Weiminli@users.noreply.github.com>
2021-06-02 17:16:57 -0400

    Wl328 weiminli/calendar UI customize (#70)
    * Add the reserve and amentity switch ui
    
    * Shade the past time table cell for calendar(user)
    
    * Revise tooltip to avoid user revise past appointment
    
    * Add tool bar to support calendar pagination and today function
    
    * Delete, save, update calendar support, switch the amenity data when switch amenity type
    
    * rebase from master

--- web/src/pages/Calendar/CalendarSchedule/CalendarHeader/index.jsx
+++ web/src/pages/Calendar/CalendarSchedule/CalendarHeader/index.jsx
@@ -1,55 +1,44 @@
-import { Menu, Dropdown, message, Button } from "antd";
+import { useState } from "react";
+import { Menu, Dropdown } from "antd";
 import { DownOutlined } from "@ant-design/icons";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import "./index.css";
-const onClick = ({ key }) => {
-  message.info(`Click on item ${key}`);
-};
 
-const menu = (
-  <Menu onClick={onClick}>
-    <Menu.Item key="1">Meeting room</Menu.Item>
-    <Menu.Item key="2">Basketball court</Menu.Item>
-    <Menu.Item key="3">Golf course</Menu.Item>
-  </Menu>
-);
-
-const menu2 = (
-  <Menu onClick={onClick}>
-    <Menu.Item key="1">Room1</Menu.Item>
-    <Menu.Item key="2">Room2</Menu.Item>
-    <Menu.Item key="3">Room3</Menu.Item>
-  </Menu>
-);
-
-const CalendarHeader = () => {
+const CalendarHeader = (props) => {
+  const { amenity, setAmenityType } = props;
+  const [room, setRoom] = useState("Room1");
+  const amenityHandler = ({ key }) => {
+    setAmenityType(key);
+  };
+  const roomHandler = ({ key }) => {
+    setRoom(key);
+  };
+  const menu = (
+    <Menu selectable={true} onClick={amenityHandler}>
+      <Menu.Item key="Meeting room">Meeting room</Menu.Item>
+      <Menu.Item key="Basketball court">Basketball court</Menu.Item>
+      <Menu.Item key="Golf course">Golf course</Menu.Item>
+    </Menu>
+  );
+  const menu2 = (
+    <Menu selectable={true} onClick={roomHandler}>
+      <Menu.Item key="Room1">Room1</Menu.Item>
+      <Menu.Item key="Room2">Room2</Menu.Item>
+      <Menu.Item key="Room3">Room3</Menu.Item>
+    </Menu>
+  );
   return (
-    <div className="claendar-header">
-      <div className="claendar-header-left">
-        <Dropdown overlay={menu} className="claendar-header-amenity">
+    <div className="calendar-header">
+      <div className="calendar-header-left">
+        <Dropdown overlay={menu} className="calendar-header-amenity">
           <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-            Meeting room <DownOutlined />
+            {amenity} <DownOutlined />
           </a>
         </Dropdown>
-        <Dropdown overlay={menu2} className="claendar-header-amenity-room">
+        <Dropdown overlay={menu2} className="calendar-header-amenity-room">
           <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-            Room1 <DownOutlined />
+            {room} <DownOutlined />
           </a>
         </Dropdown>
-      </div>
-
-      <div className="claendar-header-right">
-        <Button className="claendar-header-right-btn">Today</Button>
-        <Button
-          className="claendar-header-right-btn"
-          shape="circle"
-          icon={<LeftOutlined />}
-        />
-        <Button
-          className="claendar-header-right-btn"
-          shape="circle"
-          icon={<RightOutlined />}
-        />
       </div>
     </div>
   );

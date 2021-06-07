44d68005bf49e6c78c6dabef3badf3c571021fba
MEtoCS <chenlong890729@gmail.com>
2021-06-06 18:49:15 -0600

    add chat service

---
+++ chat/src/main/java/com/grouplive/chat/controller/ChatController.java
@@ -0,0 +1,59 @@
+package com.grouplive.chat.controller;
+
+import com.grouplive.chat.model.ChatMessage;
+import com.grouplive.chat.model.ChatNotification;
+import com.grouplive.chat.service.ChatMessageService;
+import com.grouplive.chat.service.ChatRoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ChatController {
+
+    @Autowired private SimpMessagingTemplate messagingTemplate;
+    @Autowired private ChatMessageService chatMessageService;
+    @Autowired private ChatRoomService chatRoomService;
+
+    @MessageMapping("/chat")
+    public void processMessage(@Payload ChatMessage chatMessage) {
+        var chatId = chatRoomService
+                .getChatId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true);
+        chatMessage.setChatId(chatId.get());
+
+        ChatMessage saved = chatMessageService.save(chatMessage);
+        messagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(),"/queue/messages",
+                new ChatNotification(
+                        saved.getId(),
+                        saved.getSenderId(),
+                        saved.getSenderName()));
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}/count")
+    public ResponseEntity<Long> countNewMessages(
+            @PathVariable String senderId,
+            @PathVariable String recipientId) {
+
+        return ResponseEntity
+                .ok(chatMessageService.countNewMessages(senderId, recipientId));
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<?> findChatMessages ( @PathVariable String senderId,
+                                                @PathVariable String recipientId) {
+        return ResponseEntity
+                .ok(chatMessageService.findChatMessages(senderId, recipientId));
+    }
+
+    @GetMapping("/messages/{id}")
+    public ResponseEntity<?> findMessage ( @PathVariable String id) {
+        return ResponseEntity
+                .ok(chatMessageService.findById(id));
+    }
+}
---
+++ chat/src/main/java/com/grouplive/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,12 @@
+package com.grouplive.chat.repository;
+
+import com.grouplive.chat.model.ChatRoom;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+    Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
+}
+
+

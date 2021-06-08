9e349ec14632fd089f58a3e7ae2230c76fa65253
wl328Weiminli <54866038+wl328Weiminli@users.noreply.github.com>
2021-06-08 16:31:47 -0400

    Set the login, register and add reserve UI to the Dashboard (#98)
    * Set the login, register and add reserve UI to the Dashboard
    
    * Revise the base url as environment variable

--- web/src/pages/Dashboard/index.jsx
+++ web/src/pages/Dashboard/index.jsx
@@ -1,12 +1,37 @@
 import React from "react";
-import { Card } from "antd";
+import { connect } from "react-redux";
+import { List, Card, Button } from "antd";
 import Macy from "macy";
 import "./index.less";
 class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.testRef = React.createRef();
-    this.state = {};
+    this.state = {
+      eventList: [
+        {
+          id: 1,
+          title: `BBQ`,
+          content: `Delicious BBq Event`,
+          reserve: false,
+          img: `https://images.ctfassets.net/86mn0qn5b7d0/featured-img-of-post-152543/436cf88c49f85eb46d1ab04748cbf8e6/featured-img-of-post-152543.jpg?w=1800&q=50&fm=jpg&fl=progressive`,
+        },
+        {
+          id: 2,
+          title: `Basketball game`,
+          content: `3 V 3 basketball game`,
+          reserve: false,
+          img: `https://images.crazygames.com/games/basketball-stars-2019/cover-1583231506155.png?auto=format,compress&q=75&cs=strip`,
+        },
+        {
+          id: 3,
+          title: `Hollowean`,
+          content: `Halloween evolved from the ancient Celtic holiday of Samhain.`,
+          reserve: true,
+          img: `https://ichef.bbci.co.uk/news/976/cpsprodpb/753F/production/_115151003_smallergettyimages-1184857940.jpg`,
+        },
+      ],
+    };
   }
 
   getMacy = () => {
@@ -16,6 +41,7 @@ class Dashboard extends React.Component {
       let masonry = new Macy({
         container: ".macy-container",
         trueOrder: false,
+        waitForImages: true,
         debug: true,
         margin: { x: 60, y: 30 },
         columns: 2,
@@ -23,7 +49,19 @@ class Dashboard extends React.Component {
       this.setState({ masonry });
     }
   };
+  serveAndUnserveHandler = (id) => {
+    const { eventList } = this.state;
+    const newEventList = eventList.map((ele) => {
+      if (id === ele.id) {
+        ele.reserve = !ele.reserve;
+      }
+      return ele;
+    });
+    this.setState({ eventList: newEventList });
+  };
   render() {
+    const { eventList } = this.state;
+    const reservedEvent = eventList.filter((ele) => ele.reserve);
     return (
       <div className="dashboard-wrapper">
         <div className="macy-container">
@@ -33,16 +71,40 @@ class Dashboard extends React.Component {
             title="Events"
             style={{
               width: 300,
+              height: 600,
+              overflow: "auto",
             }}
           >
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+            <List
+              itemLayout="vertical"
+              size="small"
+              dataSource={eventList}
+              renderItem={(item) => (
+                <List.Item
+                  key={item.title}
+                  actions={[
+                    <Button
+                      type="primary"
+                      onClick={() => {
+                        this.serveAndUnserveHandler(item.id);
+                      }}
+                      danger={item.reserve ? true : false}
+                    >
+                      {item.reserve ? "Unreserve" : "Reserve"}
+                    </Button>,
+                  ]}
+                  extra={<img width={272} alt="logo" src={item.img} />}
+                >
+                  <List.Item.Meta title={<div>{item.title}</div>} />
+                  {item.content}
+                </List.Item>
+              )}
+            />
           </Card>
           <Card
             hoverable
             className=".macy-item"
-            title="Recommendation news feed"
+            title="Recommendation Event"
             style={{ width: 300 }}
           >
             <p>Card content</p>
@@ -72,6 +134,38 @@ class Dashboard extends React.Component {
           <Card
             hoverable
             className=".macy-item"
+            title="Reserved"
+            style={{ width: 300 }}
+          >
+            <List
+              itemLayout="vertical"
+              size="small"
+              dataSource={reservedEvent}
+              renderItem={(item) => (
+                <List.Item
+                  key={item.title}
+                  actions={[
+                    <Button
+                      type="primary"
+                      onClick={() => {
+                        this.serveAndUnserveHandler(item.id);
+                      }}
+                      danger={item.reserve ? true : false}
+                    >
+                      {item.reserve ? "Unreserve" : "Reserve"}
+                    </Button>,
+                  ]}
+                  extra={<img width={272} alt="logo" src={item.img} />}
+                >
+                  <List.Item.Meta title={<div>{item.title}</div>} />
+                  {item.content}
+                </List.Item>
+              )}
+            />
+          </Card>
+          <Card
+            hoverable
+            className=".macy-item"
             title="Policy"
             style={{ width: 300 }}
           >
@@ -90,4 +184,9 @@ class Dashboard extends React.Component {
   }
 }
 
-export default Dashboard;
+export default connect(
+  (state) => ({
+    amenityList: state.testData,
+  }),
+  {}
+)(Dashboard);

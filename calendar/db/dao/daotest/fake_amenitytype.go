bb3b45345589e5210b773486feac412a02f4de5e
yijia-cc <bestmissjia@gmail.com>
2021-06-09 12:13:14 -0700

    Add dao test function

--- calendar/db/dao/daotest/fake_amenitytype.go
+++ calendar/db/dao/daotest/fake_amenitytype.go
@@ -1,21 +1,26 @@
 package daotest
 
-//import (
-//	"github.com/yijia-cc/grouplive/calendar/db/dao"
-//	"github.com/yijia-cc/grouplive/calendar/entity"
-//	"github.com/yijia-cc/grouplive/calendar/tx"
-//)
-//
-//var _ dao.AmenityType = (*FakeAmenityType)(nil)
-//
-//type FakeAmenityType struct {
-//	amenityTypes []entity.AmenityType
-//}
-//
-//func (f FakeAmenityType) GetAllAmenityTypes(_ tx.Transaction) ([]entity.AmenityType, error) {
-//	return f.amenityTypes, nil
-//}
-//
-//func NewFakeAmenityType(amenityTypesFixture []entity.AmenityType) FakeAmenityType {
-//	return FakeAmenityType{amenityTypes: amenityTypesFixture}
-//}
+import (
+	"github.com/graph-gophers/graphql-go"
+	"github.com/yijia-cc/grouplive/calendar/db/dao"
+	"github.com/yijia-cc/grouplive/calendar/entity"
+	"github.com/yijia-cc/grouplive/calendar/tx"
+)
+
+var _ dao.AmenityType = (*FakeAmenityType)(nil)
+
+type FakeAmenityType struct {
+	amenityTypes []entity.AmenityType
+}
+
+func (f FakeAmenityType) GetAmenityType(transaction tx.Transaction, ID graphql.ID) (entity.AmenityType, error) {
+	panic("implement me")
+}
+
+func (f FakeAmenityType) GetAllAmenityTypes(_ tx.Transaction) ([]entity.AmenityType, error) {
+	return f.amenityTypes, nil
+}
+
+func NewFakeAmenityType(amenityTypesFixture []entity.AmenityType) FakeAmenityType {
+	return FakeAmenityType{amenityTypes: amenityTypesFixture}
+}
